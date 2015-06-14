d9e44c4e4d32f8c5d6c09c2909fd70d3e217341f
Angelo Geels <spansjh@gmail.com>
2015-06-14 20:34:32 +0200

    Added ability to prefix a view with / to go to views root folder

--- classes/controller.php
+++ classes/controller.php
@@ -13,13 +13,21 @@ class Controller
 	
 	public function renderPartial($view, $options = array())
 	{
+		extract($options);
+		
 		global $nf_www_dir;
 		global $nf_cfg;
 		
-		extract($options);
-		$folder = strtolower(substr(get_class($this), 0, -strlen(__CLASS__)));
+		$inc_folder = strtolower(substr(get_class($this), 0, -strlen(__CLASS__)));
+		$inc_path = $nf_www_dir . '/' . $nf_cfg['paths']['views'];
+		if($view[0] == '/') {
+			$inc_path .= $view . '.php';
+		} else {
+			$inc_path .= '/' . $inc_folder . '/' . $view . '.php';
+		}
+		
 		ob_start();
-		include($nf_www_dir . '/' . $nf_cfg['paths']['views'] . '/' . $folder . '/' . $view . '.php');
+		include($inc_path);
 		return ob_get_clean();
 	}
 	
