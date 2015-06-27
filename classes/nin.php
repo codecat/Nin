1b513223b1a7ff9de31275a4482db5399bf49b96
Angelo Geels <spansjh@gmail.com>
2015-06-27 11:30:24 +0200

    Added Nin::timeAgo()

--- classes/nin.php
+++ classes/nin.php
@@ -65,4 +65,48 @@ class Nin
 		Nin::$current_user = $user;
 		return $user;
 	}
+
+	public static function timeAgo($oldTime, $tags = true)
+	{
+		$timeCalc = 0;
+		if(preg_match("/^[0-9]+$/", $oldTime)) {
+			$timeCalc = time() - intval($oldTime);
+		} else {
+			$timeCalc = time() - strtotime($oldTime);
+		}
+		$timeType = "s";
+		if($timeCalc >= 60) {
+			$timeType = "m";
+		}
+		if($timeCalc >= (60*60)) {
+			$timeType = "h";
+		}
+		if($timeCalc >= (60*60*24)) {
+			$timeType = "d";
+		}
+		if($timeType == "s") {
+			if($timeCalc == 1) {
+				$timeCalc .= " second ago";
+			} else {
+				$timeCalc .= " seconds ago";
+			}
+		}
+		if($timeType == "m") {
+			$mins = round($timeCalc/60);
+			$timeCalc = $mins . " minute" . ($mins != 1 ? "s" : "") . " ago";
+		}
+		if($timeType == "h") {
+			$hrs = round($timeCalc/60/60);
+			$timeCalc = $hrs . " hour" . ($hrs != 1 ? "s" : "") . " ago";
+		}
+		if($timeType == "d") {
+			$days = round($timeCalc/60/60/24);
+			$timeCalc = $days . " day" . ($days != 1 ? "s" : "") . " ago";
+		}
+		if($tags) {
+			return "<span title=\"" . $oldTime . "\">" . $timeCalc . "</span>";
+		} else {
+			return $timeCalc;
+		}
+	}
 }
