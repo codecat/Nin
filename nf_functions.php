81387d317296a03e2c104c5dc25b1d11ac25ee24
Angelo Geels <spansjh@gmail.com>
2015-09-08 14:36:31 +0200

    nf_config can now go into sub-arrays by delimiting words with periods

--- nf_functions.php
+++ nf_functions.php
@@ -469,12 +469,24 @@ function nf_sql_encode($o)
 }
 
 /**
- * Return the parameter from $nf_cfg
+ * Return the parameter from $nf_cfg['params'] with an optional default value if the param does not exist.
+ * You can use periods to delimit between arrays.
+ *
+ * For example, you can access $nf_cfg['params']['test']['foo']['bar'] by using the key:
+ *  "test.foo.bar"
  */
-function nf_param($key)
+function nf_param($key, $default = null)
 {
 	global $nf_cfg;
-	return $nf_cfg['params'][$key];
+	$keys = explode('.', $key);
+	$ret = $nf_cfg['params'];
+	foreach($keys as $k) {
+		if(!isset($ret[$k])) {
+			return $default;
+		}
+		$ret = $ret[$k];
+	}
+	return $ret;
 }
 
 function nf_xcopy($src, $dst, $ignore = array())
