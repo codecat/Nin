58c1fe176fe435579afb4b6be6df2f847ed2f81d
Melissa Geels <spansjh@gmail.com>
2022-07-09 21:59:17 +0200

    Added return param to AuthSession

--- classes/Middleware/AuthSession.php
+++ classes/Middleware/AuthSession.php
@@ -4,21 +4,35 @@ namespace Nin\Middleware;
 
 class AuthSession extends \Nin\Middleware
 {
-	private string|bool $redirect;
+	private string|false $redirect;
+	private string|false $return_param;
 
-	public function __construct(string|false $redirect = '/login')
+	public function __construct(string|false $redirect = '/login', string|false $return_param = 'r')
 	{
 		$this->redirect = $redirect;
+		$this->return_param = $return_param;
 	}
 
 	public function exec(string $action) : string|false
 	{
 		if (\Nin\Nin::uid() === false) {
-			if ($this->redirect !== false) {
-				header('Location: ' . $this->redirect);
+			if ($this->redirect === false) {
+				return false;
 			}
+
+			$location = $this->redirect;
+			if ($this->return_param !== false) {
+				if (str_contains($location, '?')) {
+					$location .= '&';
+				} else {
+					$location .= '?';
+				}
+				$location .= $this->return_param . '=' . urlencode($_SERVER['REQUEST_URI']);
+			}
+			header('Location: ' . $location);
 			return false;
 		}
+
 		return $action;
 	}
 }
